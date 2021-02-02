de04bfcd9741189208bc08d8bce2ae174a53bfb2
Dan Bellini <danbellini@Dans-MacBook-Pro.local>
2021-02-02 16:22:16 -0500

    Created iOS app

---
+++ Algorhythm/Algorhythm/AlgorhythmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorhythmApp.swift
+//  Algorhythm
+//
+//  Created by Dan Bellini on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct AlgorhythmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
